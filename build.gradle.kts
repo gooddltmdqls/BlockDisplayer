2b52508fafa841797ea02171f20b735d25b95820
Icetang0123 <1415wwwh@gmail.com>
2023-08-27 19:46:45 +0900

    something changed XD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.21"
-    `maven-publish`
+    `maven-publish` apply false
 }
 
 allprojects {
@@ -17,23 +17,24 @@ allprojects {
 subprojects {
     apply {
         plugin("org.jetbrains.kotlin.jvm")
+        plugin("maven-publish")
     }
-}
 
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/gooddltmdqls/BlockDisplayer")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
             }
         }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
         }
     }
 }
